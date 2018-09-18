bbdf7f56712581d4eb47b4187c87360cd0bb79f8
Joao Pedro Bretanha <jpbretanha@gmail.com>
2018-09-17 22:20:03 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Graphql Api for Hackernews
+
+Run `yarn`
+
+## Creating Database
+
+run `docker-compose up -d` inside `database/hackernews` to start docker prisma container. After run `prisma deploy` to start prisma server
+
+## Server
+
+Run `yarn start` to run server and `graphql playground` to open graphql playground UI
---
+++ src/resolvers/Query.js
@@ -0,0 +1,31 @@
+function feed(parent, args, context, info) {
+    const where = args.filter
+    ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter },
+        ],
+      }
+    : {}
+
+    const queriedLinks = await context.db.query.links(
+        { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+        `{ id }`,
+    )
+    const countSelectionSet = `
+        {
+            aggregate {
+                count
+            }
+        }
+    `
+    const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+    return {
+        count: linksConnection.aggregate.count,
+        linkIds: queriedLinks.map(link => link.id),
+      }
+  }
+  
+  module.exports = {
+    feed,
+  }
